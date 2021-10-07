c23046f401c8dad399a08b5e9508bbb488da8f27
Erik Gill-Fisher <erikgill98@yahoo.com>
2021-10-07 19:42:33 -0400

    Add files via upload

---
+++ Lab5-ErikGillFisher/Chapter 12/InventoryMaintenance-ErikGillFisher/InventoryMaintenance/InvItem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+
+    //referenced textbook for assignment
+    public class InvItem
+    {
+
+        private int itemNo;
+        private string description;
+        private decimal price;
+
+        public InvItem() { }
+
+        //custom constructor
+        public InvItem(int itemNo, string description, decimal price)
+        {
+            this.itemNo = itemNo;
+            this.description = description;
+            this.price = price;
+        }
+
+        //item property
+        public int ItemNo
+        {
+            get
+            {
+                return itemNo;
+            }
+            set
+            {
+                itemNo = value;
+            }
+
+        }
+
+        //descrption property
+        public string Description
+        {
+
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+
+        }
+
+        //price property
+        public decimal Price
+        {
+
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+
+        }
+
+        //GetDisplayText Method
+        public string GetDisplayText(string sep)
+        {
+            return itemNo + sep + price.ToString("c") + sep + description;
+        }
+    }
+}
---
+++ Lab5-ErikGillFisher/Chapter 12/InventoryMaintenance-ErikGillFisher/InventoryMaintenance/frmInvMaint.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryMaintenance
+{
+    public partial class frmInvMaint : Form
+	{
+		//referenced textbook for assignment
+		public frmInvMaint()
+		{
+			InitializeComponent();
+		}
+
+		// Add a statement here that declares the list of items.
+		private List<InvItem> invItems = null;
+
+		private void frmInvMaint_Load(object sender, EventArgs e)
+		{
+			// Add a statement here that gets the list of items.
+			invItems = InvItemDB.GetItems();
+			FillItemListBox();
+		}
+
+		private void FillItemListBox()
+		{
+			lstItems.Items.Clear();
+			// Add code here that loads the list box with the items in the list.
+			foreach(InvItem i in invItems)
+            {
+				lstItems.Items.Add(i.GetDisplayText("\t"));
+            }
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			// Add code here that creates an instance of the New Item form
+			// and then gets a new item from that form.
+			frmNewItem newItemForm = new frmNewItem();
+			InvItem invItem = newItemForm.GetNewItem();
+			if(invItem != null)
+            {
+				invItems.Add(invItem);
+				InvItemDB.SaveItems(invItems);
+				FillItemListBox();
+            }
+		}
+
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+			int i = lstItems.SelectedIndex;
+			if (i != -1)
+			{
+				// Add code here that displays a dialog box to confirm
+				// the deletion and then removes the item from the list,
+				// saves the list of products, and refreshes the list box
+				// if the deletion is confirmed.
+				InvItem invItem = invItems[i];
+				string mess = "Do you want to delete " + invItem.Description + "?";
+				DialogResult diag = MessageBox.Show(mess, "Confirm Delete", MessageBoxButtons.YesNo);
+				if(diag == DialogResult.Yes)
+                {
+					invItems.Remove(invItem);
+					InvItemDB.SaveItems(invItems);
+					FillItemListBox();
+				}
+			}
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
