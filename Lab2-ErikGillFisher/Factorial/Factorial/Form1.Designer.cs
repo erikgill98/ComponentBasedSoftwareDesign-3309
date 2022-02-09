998e07948803b541c4725a24611bb089676fca8b
Erik Gill-Fisher <erikgill98@yahoo.com>
2022-02-09 14:59:31 -0500

    Add files via upload

---
+++ Lab2-ErikGillFisher/Factorial/Factorial/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Factorial
+{
+    partial class FactorialCalc
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblFactorial = new System.Windows.Forms.Label();
+            this.txtFactorial = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(22, 24);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(54, 15);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "Number:";
+            // 
+            // lblFactorial
+            // 
+            this.lblFactorial.AutoSize = true;
+            this.lblFactorial.Location = new System.Drawing.Point(22, 62);
+            this.lblFactorial.Name = "lblFactorial";
+            this.lblFactorial.Size = new System.Drawing.Size(55, 15);
+            this.lblFactorial.TabIndex = 1;
+            this.lblFactorial.Text = "Factorial:";
+            // 
+            // txtFactorial
+            // 
+            this.txtFactorial.Location = new System.Drawing.Point(82, 59);
+            this.txtFactorial.Name = "txtFactorial";
+            this.txtFactorial.ReadOnly = true;
+            this.txtFactorial.Size = new System.Drawing.Size(157, 23);
+            this.txtFactorial.TabIndex = 4;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(82, 21);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(75, 23);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(84, 103);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(166, 103);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FactorialCalc
+            // 
+            this.AcceptButton = this.btnCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(255, 145);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtFactorial);
+            this.Controls.Add(this.lblFactorial);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "FactorialCalc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Factorial Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblFactorial;
+        private System.Windows.Forms.TextBox txtFactorial;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Lab2-ErikGillFisher/Factorial/Factorial/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial
+{
+    public partial class FactorialCalc : Form
+    {
+        public FactorialCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            //converting txtNumber to a 64bit int -- setting equal to num
+            long num = Convert.ToInt64(txtNumber.Text);
+            //starting total at 1
+            long total = 1;
+
+            while (num > 0 && num <= 20)
+            {
+                //setting total = each run through of 1,2,3 etc
+                total *= num;
+                //decreasing num by 1
+                num--;
+            }
+            //printing number into factorial textbox
+            txtFactorial.Text = total.ToString("N0");
+
+
+            //focusing on number textbox
+            txtNumber.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
