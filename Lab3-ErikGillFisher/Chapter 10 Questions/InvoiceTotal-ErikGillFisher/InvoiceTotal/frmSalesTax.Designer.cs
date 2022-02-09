6208d9da29e213b6968ada25240aa4d3ddd01b6c
Erik Gill-Fisher <erikgill98@yahoo.com>
2022-02-09 15:12:34 -0500

    Add files via upload

---
+++ Lab3-ErikGillFisher/Chapter 10 Questions/InvoiceTotal-ErikGillFisher/InvoiceTotal/frmSalesTax.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace InvoiceTotal
+{
+    partial class frmSalesTax
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTaxPct = new System.Windows.Forms.Label();
+            this.txtTaxPct = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTaxPct
+            // 
+            this.lblTaxPct.AutoSize = true;
+            this.lblTaxPct.Location = new System.Drawing.Point(12, 9);
+            this.lblTaxPct.Name = "lblTaxPct";
+            this.lblTaxPct.Size = new System.Drawing.Size(75, 13);
+            this.lblTaxPct.TabIndex = 0;
+            this.lblTaxPct.Text = "Sales Tax pct:";
+            // 
+            // txtTaxPct
+            // 
+            this.txtTaxPct.Location = new System.Drawing.Point(93, 6);
+            this.txtTaxPct.Name = "txtTaxPct";
+            this.txtTaxPct.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxPct.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(24, 32);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(105, 32);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmSalesTax
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(204, 82);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtTaxPct);
+            this.Controls.Add(this.lblTaxPct);
+            this.Name = "frmSalesTax";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmSalesTax";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTaxPct;
+        private System.Windows.Forms.TextBox txtTaxPct;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Lab3-ErikGillFisher/Chapter 10 Questions/InvoiceTotal-ErikGillFisher/InvoiceTotal/frmSalesTax.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceTotal
+{
+    public partial class frmSalesTax : Form
+    {
+        public frmSalesTax()
+        {
+            InitializeComponent();
+        }
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                
+                decimal salesTax = Convert.ToDecimal(txtTaxPct.Text);
+                //sets tag to sale tax
+                Tag = salesTax;
+                this.Close();
+            }
+            else
+            {
+                txtTaxPct.Clear();
+                txtTaxPct.Focus();
+            }
+        }
+
+        public bool IsValidData()
+        {
+            return
+                IsPresent(txtTaxPct, "Subtotal") &&
+                IsDecimal(txtTaxPct, "Subtotal") &&
+                IsWithinRange(txtTaxPct, "Input", 0, 10);
+        }
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        public bool IsDecimal(TextBox textBox, string name)
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a decimal number.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsWithinRange(TextBox textBox, string name,
+            decimal min, decimal max)
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number <= min || number >= max)
+            {
+                MessageBox.Show(name + " must be between " + min +
+                    " and " + max + ".", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+    }
+}
